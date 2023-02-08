b74d4b4fb828d33fd873902199d791a4e4453c8f
Rodrigo322 <rodrigollrdglucas@gmail.com>
2023-02-08 15:05:46 -0300

    porta modificada

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
+const PORT = 3333
 
 app.use(cors()); 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log('server listening on'));
+app.listen(process.env.PORT || PORT, () => console.log('server listening on'));
